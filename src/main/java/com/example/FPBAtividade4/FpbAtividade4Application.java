075b4569f1a7903b964f9d4eafa5f04e8a31b0c3
Arthur Guimarães <art.vrgm@gmail.com>
2024-06-21 21:08:10 -0300

    First commit

---
+++ src/main/java/com/example/FPBAtividade4/FpbAtividade4Application.java
@@ -0,0 +1,9 @@
+package com.example.FPBAtividade4;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class FpbAtividade4Application extends SpringBootServletInitializer {
+
+}
